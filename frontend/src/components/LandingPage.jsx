4fab7694776c765d7191ef3ed04ffebe8b0d4241
OnlyFirSchool <n1m41ffl168@gmail.com>
2019-10-28 12:32:27 -0700

    Added jumbo to landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 import DocumentTitle from "react-document-title";
 //import "./App.css";
@@ -10,17 +11,30 @@ import {
   Redirect
 } from "react-router-dom";
 
+import './LandingPage.css';
 // import NavBar from "./NavBar";
 import Carousel from "../LandingPage/Carousel.jsx";
 
 class LandingPage extends Component {
   state = {
-    username: ''
+    userFirstName: '',
+    userLastName: '',
+    userEmail: '',
+    userAge: ''
   }
 
+  // componentDidMount () {
+  //   axios.get('localhost:3000/users/me')
+  //   .then(res => {
+  //     this.setState({userFirstName: res.firstName})
+  //   });
+  // }
+
   render() {
+    const user = "Dynamic User!";
     return (
       <div>
+        <h1 id="jum">Welcome {user}!</h1>
         <Carousel />
       </div>
     );
