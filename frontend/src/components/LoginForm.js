19a3aca156cd34dd26cf11a5d224f631ddb165fb
OnlyFirSchool <n1m41ffl168@gmail.com>
2019-10-28 12:33:00 -0700

    Implemented form validation template without redux

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -12,22 +12,71 @@ import{
   Col,
   Button
 } from 'reactstrap';
+import axios from 'axios';
 
 
 const styles = {
     marginTop: '150px',
 };
 
+const initialState = {
+  email: "",
+  password: "",
+  userTokens: [],
+  showErrorMessage: false,
+  userHasToken: false,
+  verified: false,
+
+};
 class LoginForm extends Component {
-    state = {
-      email: '',
-      password: '',
-      userHasToken: false,
-      verified: false,
-      userTokens: [],
-      emailError: ""
-    }
+    state = initialState;
+
+    validateCases = () => {
+      let emailError="";
+      let passwordError ='';
+     
+      if (!this.state.email.includes('@uci.edu') || this.state.password.length < 8)
+      {      
+        console.log('invalid email');
+        emailError ='invalid email';
+        this.setState({showErrorMessage: true});
+        console.log(this.state.showErrorMessage);
+        return false;
+      }
+
+      else if (!this.state.password) {
+        console.log('no password inputed');
+        this.setState({showErrorMessage: true});
+        return false;
+      }
+      this.state.showErrorMessage = false;
+      return true;
+    };
+
+    handleInputChange = e => {
+      this.setState( {[e.target.name]: e.target.value} );
+      console.log(` target value is ${e.target.value} and name is ${e.target.name}`);
+    };
+
+    submitHandler = e => {
+      e.preventDefault();
+      const valid = this.validateCases();
+      if (valid ) {   {/*&& this.state.verified */}
+        console.log(this.state);
+        this.setState(initialState);
+      }
+    };
+
     render() {
+      let errorMessage = null;
+      if (this.state.showErrorMessage) {
+        errorMessage = ( 
+          <div className='errorMessage'>
+            *Invalid email or password
+          </div>
+    
+        );
+      }
       return (
         <div className='styles'>
           <Container>
@@ -35,20 +84,22 @@ class LoginForm extends Component {
               <Col md='4'></Col>
               <Col md='4' className='contain'>
                 <h1 id='idH1'>Sign in to On The House</h1>
-                <Form>
+                {errorMessage}
+                <Form onSubmit={this.submitHandler}>
                   <FormGroup row> 
                     <Col>
                       <Label className='d-flex justify-content-start' for="uciEmail">Email</Label>
-                      <Input type="email" name="email" id="uciEmail" placeholder="Enter UCI email" />
+                      <Input type="email" name="email" value={this.state.name} id="uciEmail" onChange={this.handleInputChange} placeholder="Enter UCI email" />
                     </Col>
                   </FormGroup>
                   <FormGroup row> 
                     <Col>
                       <Label className='d-flex justify-content-start' for="userPassword">Password</Label>
-                      <Input type="password" name="password" id="userPassword" placeholder="Enter password"/>
+                      <Input type="password" name="password" value={this.state.password} id="userPassword" onChange={this.handleInputChange} placeholder="Enter password"/> 
+                      {/* value={this.state.password} */}
                     </Col>
                   </FormGroup>
-                  <Button className='d-flex justify-content-start' action="/">Sign In</Button>
+                  <Button type='submit' className='d-flex justify-content-start' action="/">Sign In</Button>
                 </Form>
               </Col>
               <Col xs='4'></Col>
