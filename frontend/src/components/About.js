ecb51fa7c0e651e60b570152dd60b0654d8a066d
tyler-Mos <tyler_mos@yahoo.com>
2019-10-28 22:39:04 -0700

    Landing Page 3.0

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -27,7 +27,7 @@ class About extends Component {
         <h1 id="jum"></h1>
         <div id="top">
           <div id="header">
-            <h2>Meet "On The House"</h2>
+            <h2 id="htwo">Meet "On The House"</h2>
             <h4>
               A handtailored site to help students with all their sublease
               needs.
@@ -36,18 +36,15 @@ class About extends Component {
         </div>
         <div id="bot">
           <p>
-            Our goal in creating "On The House" was to devlop an app that would
-            be hand tailored to the student population of UCI. We knew that
-            students were tired of having to tediously search through third
-            party sites to be able to search and post their subleases. We wanted
-            to create an app that would serve as a Hub to remedy this problem.
-            By using validation services UCI students and faculty will now be
-            able to easily search and create posts for their subleases. There
-            are 9 student housing communities on the UCI campus, to meet the
-            needs of a diverse student population. See the Undergraduate and
-            Graduate and Family Housing fliers for an overview of our
-            communities. To identify your specific housing options, indicate
-            your student status below and click on the Submit button.
+            Our goal in creating "On The House" was to solve a common problem
+            facing the general student population of UCI. We knew that students
+            were tired of having to tediously search through third party sites
+            to be able to search and post their subleases. Our primary goal in
+            creating this app would be to remedey this situation by using
+            validation services. By using these services UCI students and
+            faculty will now be able to easily search and create posts for their
+            subleases, without have to worry about the identity of the other
+            pary.
           </p>
         </div>
       </div>
