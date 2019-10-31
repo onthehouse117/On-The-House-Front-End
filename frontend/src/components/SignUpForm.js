12965f03f7ada36d942bcdf47d8ce53f2477a96b
OnlyFirSchool <n1m41ffl168@gmail.com>
2019-10-30 18:31:42 -0700

    Added a sign in option under sign up form

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -112,45 +112,48 @@ class SignUpForm extends Component {
             <Container>
               <div className='row'>
                 <Col md='4'></Col>
-                <Col md='4' className='contain'>
-                  <h1 id='idH1'>Register to On The House</h1>
-                  {errorMessage}
-                  {this.state.msg ? <Alert color="danger">{ this.state.msg }</Alert> : null}
-                  <Form onSubmit={this.onSubmit}>
-                    <FormGroup row> 
+                <Col md='4'>
+                  <div className='contain'>
+                    <h1 id='idH1'>Register to On The House</h1>
+                    {errorMessage}
+                    {this.state.msg ? <Alert color="danger">{ this.state.msg }</Alert> : null}
+                    <Form onSubmit={this.onSubmit}>
+                      <FormGroup row> 
+                        <Col>
+                          <Label className='d-flex justify-content-start' for="firstName">First Name</Label>
+                          <Input type="text" name="firstName" id="firstName" onChange={this.onChange} placeholder="First Name" />
+                        </Col>
+                      </FormGroup>
+                      <FormGroup row> 
+                        <Col>
+                          <Label className='d-flex justify-content-start' for="lastName">Last Name</Label>
+                          <Input type="text" name="lastName" id="lastName" onChange={this.onChange} placeholder="Last Name" />
+                        </Col>
+                      </FormGroup>
+                      <FormGroup row> 
                       <Col>
-                        <Label className='d-flex justify-content-start' for="firstName">First Name</Label>
-                        <Input type="text" name="firstName" id="firstName" onChange={this.onChange} placeholder="First Name" />
+                          <Label className='d-flex justify-content-start' for="enteredDate">Date of Birth</Label>
+                          <Input type="date" name="DOB" id="enteredDate" onChange={this.onChangeDate} />
                       </Col>
-                    </FormGroup>
-                    <FormGroup row> 
+                      </FormGroup>
+                      <FormGroup row> 
                       <Col>
-                        <Label className='d-flex justify-content-start' for="lastName">Last Name</Label>
-                        <Input type="text" name="lastName" id="lastName" onChange={this.onChange} placeholder="Last Name" />
+                          <Label className='d-flex justify-content-start' for="uciEmail">Email</Label>
+                          <Input type="email" name="email" value={this.state.name} id="uciEmail" onChange={this.onChange} placeholder="Enter UCI email" />
                       </Col>
-                    </FormGroup>
-                    <FormGroup row> 
-                    <Col>
-                        <Label className='d-flex justify-content-start' for="enteredDate">Date of Birth</Label>
-                        <Input type="date" name="DOB" id="enteredDate" onChange={this.onChangeDate} />
-                    </Col>
-                    </FormGroup>
-                    <FormGroup row> 
-                    <Col>
-                        <Label className='d-flex justify-content-start' for="uciEmail">Email</Label>
-                        <Input type="email" name="email" value={this.state.name} id="uciEmail" onChange={this.onChange} placeholder="Enter UCI email" />
-                    </Col>
-                    </FormGroup>
-                    <FormGroup row> 
-                    <Col>
-                        <Label className='d-flex justify-content-start' for="userPassword">Password</Label>
-                        <Input type="password" name="password" value={this.state.password} id="userPassword" onChange={this.onChange} placeholder="Enter password" /> 
-                        <p class="pwHint">Must contain at least one upper case letter, one lower case letter, one number, and one special character</p>
-                        {/* value={this.state.password} */}
-                    </Col>
-                    </FormGroup>
-                    <Button type='submit' className='d-flex justify-content-start' action="/">Register</Button>
-                  </Form>
+                      </FormGroup>
+                      <FormGroup row> 
+                      <Col>
+                          <Label className='d-flex justify-content-start' for="userPassword">Password</Label>
+                          <Input type="password" name="password" value={this.state.password} id="userPassword" onChange={this.onChange} placeholder="Enter password" /> 
+                          <p class="pwHint">Must contain at least one upper case letter, one lower case letter, one number, and one special character</p>
+                          {/* value={this.state.password} */}
+                      </Col>
+                      </FormGroup>
+                      <Button type='submit' className='d-flex justify-content-start' action="/">Register</Button>
+                    </Form>
+                  </div>
+                  <a href="/users/loginpage" style={{margintop: '5rem'}}>Already a user? Sign In</a>
                 </Col>
                 <Col xs='4'></Col>
               </div>
