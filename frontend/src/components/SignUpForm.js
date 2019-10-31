8c223865f36ae2e94cd1261df5a765229a6c2db6
tyler-Mos <tyler_mos@yahoo.com>
2019-10-31 12:50:50 -0700

    front end verification, commented out old code

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,27 +1,40 @@
 import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import "./SignUpForm.css";
-import PropTypes from 'prop-types';
-import{
-  Form, 
+import PropTypes from "prop-types";
+import {
+  Form,
   FormGroup,
   Label,
   Input,
+  FormText,
   Container,
   Row,
   Col,
   Button,
   Alert
-  } from 'reactstrap';
+} from "reactstrap";
 
-import { register } from '../actions/authActions';
+import { register } from "../actions/authActions";
 
 const styles = {
-  marginTop: '150px'
+  marginTop: "150px"
 };
-
+const initialState = {
+  firstName: "",
+  lastName: "",
+  DOB: null,
+  email: "",
+  password: "",
+  msg: null,
+  userTokens: [],
+  showErrorMessage: false,
+  userHasToken: false,
+  verified: false,
+  errorMsg: ""
+};
 class SignUpForm extends Component {
-  state = {
+  /*state = {
     firstName: '',
     lastName: '',
     DOB: null,
@@ -31,76 +44,91 @@ class SignUpForm extends Component {
     userTokens: [],
     showErrorMessage: false,
     userHasToken: false,
-    verified: false
-  }
+    verified: false,
+    errorMsg=""
+  }*/
+  state = initialState;
 
   static propTypes = {
     isAuthenticated: PropTypes.bool,
     error: PropTypes.object.isRequired,
-    register: PropTypes.func.isRequired,
-  }
+    register: PropTypes.func.isRequired
+  };
 
+  validateCases = () => {
+    if (!this.state.firstName) {
+      console.log("no first name inputed");
+      this.setState({ showErrorMessage: true });
+      this.setState({ errorMsg: "1" });
 
-  // validateCases = () => {
-  //   if (!this.state.firstName) {
-  //     console.log("no first name inputed");
-  //     this.setState({ showErrorMessage: true });
-  //     return false;
-  //   } else if (!this.state.lastName) {
-  //     console.log("no last name inputed");
-  //     this.setState({ showErrorMessage: true });
-  //     return false;
-  //   } else if (!this.state.email.includes("@uci.edu")) {
-  //     console.log("invalid email");
-  //     this.setState({ showErrorMessage: true });
-  //     console.log(this.state.showErrorMessage);
-  //     return false;
-  //   } else if (!this.state.password || this.state.password.length < 8) {
-  //     console.log("no password inputed");
-  //     this.setState({ showErrorMessage: true });
-  //     return false;
-  //   } /*else if( )
-  //     this.state.showErrorMessage = false;
-  //     return true;*/
-  // };
+      return false;
+    } else if (!this.state.lastName) {
+      console.log("no last name inputed");
+      this.setState({ showErrorMessage: true });
+      this.setState({ errorMsg: "2" });
+      return false;
+    } else if (!this.state.email.includes("@uci.edu")) {
+      console.log("invalid email");
+      this.setState({ showErrorMessage: true });
+      this.setState({ errorMsg: "3" });
+      console.log(this.state.showErrorMessage);
+      return false;
+    } else if (!this.state.password || this.state.password.length < 8) {
+      console.log("no password inputed");
+      this.setState({ showErrorMessage: true });
+      this.setState({ errorMsg: "4" });
+
+      return false;
+    }
+    //   } /*else if( )
+    //     this.state.showErrorMessage = false;
+    //     return true;*/
+  };
 
   componentDidUpdate(prevProps) {
     const { error, isAuthenticated } = this.props;
-    if(error !== prevProps.error)
-    {
+    if (error !== prevProps.error) {
       //register error
-      if(error.id === 'REGISTER_FAIL') {
-        if (error.msg.name === "MongoError")
-        {
-          this.setState({msg: "User already exists!"});
+      if (error.id === "REGISTER_FAIL") {
+        if (error.msg.name === "MongoError") {
+          this.setState({ msg: "User already exists!" });
+        } else {
+          this.setState({ msg: error.msg.message });
         }
-        else {
-          this.setState({msg: error.msg.message});
-        }
-      }
-      else {
-        this.setState( {msg: null});
+      } else {
+        this.setState({ msg: null });
       }
     }
 
     //If user authenticated, redirect to verify page.
-    if(isAuthenticated) {
+    if (isAuthenticated) {
       const { history } = this.props;
-      history.push('/verification');
+      history.push("/verification");
+    }
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    const valid = this.validateCases();
+    if (valid) {
+      {
+        /*&& this.state.verified */
+      }
+      console.log(this.state);
+      this.setState(initialState);
     }
   };
-
   onChange = e => {
-    console.log(typeof(e.target.value))
-    this.setState({ [e.target.name]: e.target.value});
+    console.log(typeof e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  onChangeDate = e =>{
+  onChangeDate = e => {
     const d = new Date(e.target.value);
     // console.log(`d is ${d}`);
     console.log(e.target.name);
-    this.setState({ [e.target.name]: d});
-  }
+    this.setState({ [e.target.name]: d });
+  };
 
   onSubmit = e => {
     e.preventDefault();
@@ -109,100 +137,173 @@ class SignUpForm extends Component {
 
     //New user created
     const newUser = {
-        firstName,
-        lastName,
-        DOB,
-        email,
-        password
+      firstName,
+      lastName,
+      DOB,
+      email,
+      password
     };
 
     //Send new user object to register action and JSON request body
     this.props.register(newUser);
-  }
-
+  };
 
   render() {
     let errorMessage = null;
-    if (this.state.showErrorMessage) {
-      errorMessage = ( 
-        <div className='errorMessage'>
-          *Invalid email or password
-        </div>
-      );
+    if (this.state.showErrorMessage && this.state.errorMsg === "1") {
+      errorMessage = <div className="errorMessage">*Invalid first name</div>;
+    } else if (this.state.showErrorMessage && this.state.errorMsg === "2") {
+      errorMessage = <div className="errorMessage">*Invalid last name</div>;
+    } else if (this.state.showErrorMessage && this.state.errorMsg === "3") {
+      errorMessage = <div className="errorMessage">*Invalid email</div>;
+    } else if (this.state.showErrorMessage && this.state.errorMsg === "4") {
+      errorMessage = <div className="errorMessage">*Invalid email</div>;
+    } else if (this.state.showErrorMessage && this.state.errorMsg === "5") {
+      errorMessage = <div className="errorMessage">*Invalid password</div>;
+    } else if (this.state.showErrorMessage && this.state.errorMsg === "6") {
+      errorMessage = <div className="errorMessage">*Invalid DOB</div>;
     }
     return (
-      <div className='styles'>
+      /* <Alert color="danger">{this.state.msg}</Alert>
+                {this.state.msg ? () : null}
+                <Form onSubmit={this.onSubmit}>*/
+      <div className="styles">
         <Container>
-          <div className='row'>
-            <Col md='4'></Col>
-            <Col md='4'>
-              <div className='contain'>
-                <h1 id='idH1'>Register to On The House</h1>
+          <div className="row">
+            <Col md="4"></Col>
+            <Col md="4">
+              <div className="contain">
+                <h1 id="idH1">Register to On The House</h1>
                 {errorMessage}
-                {this.state.msg ? <Alert color="danger">{ this.state.msg }</Alert> : null}
-                <Form onSubmit={this.onSubmit}>
-                  <FormGroup row> 
+                <Form onSubmit={this.submitHandler}>
+                  <FormGroup row>
                     <Col>
-                      <Label className='d-flex justify-content-start' for="firstName">First Name</Label>
-                      <Input type="text" name="firstName" id="firstName" onChange={this.onChange} placeholder="First Name" />
+                      <Label
+                        className="d-flex justify-content-start"
+                        for="firstName"
+                      >
+                        First Name
+                      </Label>
+                      <Input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        onChange={this.onChange}
+                        placeholder="First Name"
+                      />
                     </Col>
                   </FormGroup>
-                  <FormGroup row> 
+                  <FormGroup row>
                     <Col>
-                      <Label className='d-flex justify-content-start' for="lastName">Last Name</Label>
-                      <Input type="text" name="lastName" id="lastName" onChange={this.onChange} placeholder="Last Name" />
+                      <Label
+                        className="d-flex justify-content-start"
+                        for="lastName"
+                      >
+                        Last Name
+                      </Label>
+                      <Input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        onChange={this.onChange}
+                        placeholder="Last Name"
+                      />
                     </Col>
                   </FormGroup>
-                  <FormGroup row> 
-                  <Col>
-                      <Label className='d-flex justify-content-start' for="enteredDate">Date of Birth</Label>
-                      <Input type="date" name="DOB" id="enteredDate" onChange={this.onChangeDate} />
-                  </Col>
+                  <FormGroup row>
+                    <Col>
+                      <Label
+                        className="d-flex justify-content-start"
+                        for="enteredDate"
+                      >
+                        Date of Birth
+                      </Label>
+                      <Input
+                        type="date"
+                        name="DOB"
+                        id="enteredDate"
+                        onChange={this.onChangeDate}
+                      />
+                    </Col>
                   </FormGroup>
-                  <FormGroup row> 
-                  <Col>
-                      <Label className='d-flex justify-content-start' for="uciEmail">Email</Label>
-                      <Input type="email" name="email" value={this.state.name} id="uciEmail" onChange={this.onChange} placeholder="Enter UCI email" />
-                  </Col>
+                  <FormGroup row>
+                    <Col>
+                      <Label
+                        className="d-flex justify-content-start"
+                        for="uciEmail"
+                      >
+                        Email
+                      </Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        value={this.state.name}
+                        id="uciEmail"
+                        onChange={this.onChange}
+                        placeholder="Enter UCI email"
+                      />
+                    </Col>
                   </FormGroup>
-                  <FormGroup row> 
-                  <Col>
-                      <Label className='d-flex justify-content-start' for="userPassword">Password</Label>
-                      <Input type="password" name="password" value={this.state.password} id="userPassword" onChange={this.onChange} placeholder="Enter password" /> 
-                      <p class="pwHint">Must contain at least one upper case letter, one lower case letter, one number, and one special character</p>
+                  <FormGroup row>
+                    <Col>
+                      <Label
+                        className="d-flex justify-content-start"
+                        for="userPassword"
+                      >
+                        Password
+                      </Label>
+                      <Input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        id="userPassword"
+                        onChange={this.onChange}
+                        placeholder="Enter password"
+                      />
+                      <p class="pwHint">
+                        Must contain at least one upper case letter, one lower
+                        case letter, one number, and one special character
+                      </p>
                       {/* value={this.state.password} */}
-                  </Col>
+                    </Col>
                   </FormGroup>
-                  <Button type='submit' className='d-flex justify-content-start' action="/">Register</Button>
+                  <Button
+                    type="submit"
+                    className="d-flex justify-content-start"
+                    action="/"
+                  >
+                    Register
+                  </Button>
                 </Form>
               </div>
-              <a href="/users/loginpage" style={{margintop: '5rem'}}>Already a user? Sign In</a>
+              <a href="/users/loginpage" style={{ margintop: "5rem" }}>
+                Already a user? Sign In
+              </a>
             </Col>
-            <Col xs='4'></Col>
+            <Col xs="4"></Col>
           </div>
         </Container>
       </div>
-    )
+    );
   }
 }
 
 const mapStatetoProps = state => ({
-    // TEMPLATE
-    // propYouWantInserted : state.ItemName,
-    authAction: state.auth,
-    isAuthenticated: state.auth.isAuthenticated,
-    error: state.error
-
+  // TEMPLATE
+  // propYouWantInserted : state.ItemName,
+  authAction: state.auth,
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error
 });
 
 const mapDispatchToProps = state => ({
-    // TEMPLATE
-    // dispatchName: Parameter =>
-    //   dispatch({ type: "ActionName", Parameter }),
+  // TEMPLATE
+  // dispatchName: Parameter =>
+  //   dispatch({ type: "ActionName", Parameter }),
 });
 
 export default connect(
   mapStatetoProps,
-//   mapDispatchToProps,
+  //   mapDispatchToProps,
   { register }
 )(SignUpForm);
