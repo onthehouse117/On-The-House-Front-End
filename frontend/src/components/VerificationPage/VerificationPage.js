3d2d992edf1eb93376dc2191981bc81ca94ace84
OnlyFirSchool <n1m41ffl168@gmail.com>
2019-11-03 22:37:32 -0800

    Verification page will redirect after 5 seconds

--- frontend/src/components/VerificationPage/VerificationPage.js
+++ frontend/src/components/VerificationPage/VerificationPage.js
@@ -1,14 +1,26 @@
 import React, { Component, useState } from "react";
+import { Link, Redirect } from 'react-router-dom';
 
 class VerificationPage extends Component {
+    state ={
+        redirect: false
+    }
+    componentDidMount() {
+        this.id = setTimeout(() => this.setState({redirect: true}), 5000);
+    }
+    componentWillUnmount() {
+        clearTimeout(this.id);
+    }
+
     render() { 
         const styles = {
             marginTop: '150px'
         };
-        return (
+        return this.state.redirect ? <Redirect to="/"></Redirect>
+        : (
             <div style= {styles}>
                 <h1>Please check your email for a confirmation link.</h1>
-                <a href="/">Click this to go back home (Going to work on implemention: user must be verified to access posts)</a>
+                <span>(</span><Link to="/">Click this link if you are not redirected in 5 seconds</Link><span>)</span>
             </div>
         )
     };
