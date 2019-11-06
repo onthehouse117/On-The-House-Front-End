5cd36df69a25fb7cb145823f55e105a914a6423e
Bruce Lee <brucelee92371@gmail.com>
2019-11-06 09:42:31 -0800

    Revert "started contact page"
    This reverts commit b6307ff41bc5796730a05d43d2088306ce39c44d.

--- frontend/src/routers/Routers.js
+++ frontend/src/routers/Routers.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import DocumentTitle from "react-document-title";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+  } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import SubLease from "../components/SubLease";
 import LoginForm from "../components/LoginForm";
@@ -14,37 +14,34 @@ import LandingPage from "../components/LandingPage";
 import PostTable from "../components/PostTable";
 import Posts from "../components/Posts";
 import About from "../components/About";
-import ContactForm from "../components/ContactForm";
 import VerificationPage from "../components/VerificationPage/VerificationPage";
 import VerificationStatus from "../components/VerificationPage/VerificationStatus";
 import PrivateRoute from './PrivateRoute';
 
-class AllRouters extends Component {
-  redirectToPostTable = e => {
-    return (
-      <Redirect to='/posttable' />
-    )
+class AllRouters extends Component { 
+  redirectToPostTable = e => { return (
+    <Redirect to='/posttable' />
+  )
   }
 
   render() {
     return (
       <div>
         <Router>
-          <DocumentTitle title="On The House"></DocumentTitle>
-          <Switch>
+            <DocumentTitle title="On The House"></DocumentTitle>
+            <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route exact path="/About" component={About} />
-            <Route exact path="/ContactForm" component={ContactForm} />
             <Route exact path="/users/signup" component={SignUpForm} />
             <Route exact path="/verification" component={VerificationPage} />
             <Route path="/verify" component={VerificationStatus} />
             <Route exact path="/users/loginpage" component={LoginForm} />
             <Route exact path="/SubLease" component={SubLease} />
-            <PrivateRoute path='/posttable' component={PostTable} />
+            <PrivateRoute path='/posttable' component={PostTable}  />
             {/* <Route exact path="/posttable" component={PostTable} /> */}
             <Route exact path="/post" component={Posts} />
             <Redirect to="/" />
-          </Switch>
+            </Switch>
         </Router>
       </div>
     );
