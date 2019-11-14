611611250e5471df33c53f8d07d26f6318aca518
Bruce Lee <brucelee92371@gmail.com>
2019-11-13 20:01:33 -0800

    Comments NEed Display

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import image from "../images/image.jpg";
 import "./posts.css";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import { Media } from "reactstrap";
+import { Media, Button } from "reactstrap";
 import axios from "axios";
 
 class Posts extends Component {
@@ -13,7 +13,6 @@ class Posts extends Component {
 
   clickedComment() {
     //Do all axios calls in the postActions!!
-    console.log("Hello World")
     const config = {
       headers: {
         crossDomain: true,
@@ -23,7 +22,7 @@ class Posts extends Component {
 
     const body = JSON.stringify({});
     try {
-      axios.get("/comments/" + this.props.postData._id, body, config).then(res => {
+      axios.get("/comments/" + this.props.postData._id, config).then(res => {
         this.setState({
           comments: res.data
         });
@@ -31,10 +30,10 @@ class Posts extends Component {
     } catch (e) {
       console.log(e);
     }
-    console.log("comments:", this.state.posts);
   }
 
   render() {
+    console.log(this.state.comments)
     return (
         <div id="wrapper">
         <header class="cf">
@@ -48,26 +47,30 @@ class Posts extends Component {
         <img class="img-content" src={image}></img>
         <div class="action">
           <div class="like">
-            <Link>
-              <img src="https://1.bp.blogspot.com/-qns_lZPjg0I/VWY2dO1HN-I/AAAAAAAACVA/akLTMY7RJSk/s1600/Thumbs-up-facebook-icon-small.png" alt="thumbs up"></img>
+            <Button color = "link" onClick = {() => console.log("Liked")}>
+              <Media object src = {"https://1.bp.blogspot.com/-qns_lZPjg0I/VWY2dO1HN-I/AAAAAAAACVA/akLTMY7RJSk/s1600/Thumbs-up-facebook-icon-small.png"}></Media>
               <p>Like</p>
-            </Link>
+            </Button>
           </div>
 
           <div class="comment">
-            <Link to = "#" onClick = {this.clickedComment()}>
-                <Media>
-
-                </Media>
-              <img src="https://s0.wp.com/wp-content/themes/vip/facebook-groups/img/message_icon.png"></img>
+            <Button color = "link" onClick = {() => 
+              this.clickedComment()}>
+              <Media object src = {"https://s0.wp.com/wp-content/themes/vip/facebook-groups/img/message_icon.png"}></Media>
               <p>Comment</p>
-            </Link>
+              {/* <Media>
+                {this.state.comments[0]}
+              </Media> */}
+            </Button>
           </div>
         </div>
         </div>
+
     );
   }
 }
+
+
 
 const mapStatetoProps = state => ({
   token: state.auth.token,
