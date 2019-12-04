68ea0402f3b890aebb712fc03ded1136cfc3eeec
Giorgio Atanasov <giorgioatanasov34@yahoo.com>
2019-12-03 19:16:40 -0800

    Added netlify dependencies

---
+++ frontend/build/precache-manifest.3e6c2f4b837011c9ed1085f0a1c03ddf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283c7d18a3c409212ba88e9c44a0c4d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b53b3f758e5ec4e27592",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a50ab11f6b0455bff5cb",
+    "url": "/static/css/main.9ddf1dd4.chunk.css"
+  },
+  {
+    "revision": "b53b3f758e5ec4e27592",
+    "url": "/static/js/2.32bd4077.chunk.js"
+  },
+  {
+    "revision": "a50ab11f6b0455bff5cb",
+    "url": "/static/js/main.d5248056.chunk.js"
+  },
+  {
+    "revision": "55cbba3bc670a3dcb9bf",
+    "url": "/static/js/runtime-main.75ceb4d7.js"
+  },
+  {
+    "revision": "6c23a8dccf0e03781527aa2d76d32e09",
+    "url": "/static/media/OnTheHouseLogo.6c23a8dc.png"
+  },
+  {
+    "revision": "e80a5ae828fc5c4f380bc58c08270363",
+    "url": "/static/media/bg-login.e80a5ae8.jpg"
+  },
+  {
+    "revision": "33ee9ff61c061072adbbcc5b1bafa444",
+    "url": "/static/media/image.33ee9ff6.jpg"
+  }
+]);
